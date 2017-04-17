579050c13bdbef8dc43085624c89081aea031fb0
KennethJanssen <Kenneth.Janssen@smail.rasmussen.edu>
2017-04-16 21:09:17 -0500

    Updates to CSS Theme

---
+++ js/project.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+
+
+/* https://github.com/viljamis/ResponsiveSlides.js
+Licensed under the MIT license.
+Copyright (c) 2011-2012 Viljami Salminen, http://viljamis.com/ 
+Used to create responsive image slider for main page
+*/
+
+
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: false,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
